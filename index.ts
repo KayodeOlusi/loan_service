47e6cbb1b38362ac5881baa954b84d263f925da3
KayodeOlusi <kayodeolusi@gmail.com>
2025-01-28 15:46:43 +0100

    basic server

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import http from "http";
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("<h1>Hello, World!</h1>");
-  }
-});
-server.listen(3000, () => console.log("Server running"));
+http
+  .createServer(function (req, res) {
+    if (req.url === "/") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end();
+    }
+  })
+  .listen(8000)
