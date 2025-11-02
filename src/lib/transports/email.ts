b347b60ee6a10b88c56be86bf58329d73bcc204e
KayodeOlusi <kayodeolusi@gmail.com>
2025-11-02 10:23:07 +0100

    fix email connection error

--- src/lib/transports/email.ts
+++ src/lib/transports/email.ts
@@ -1,14 +1,13 @@
 import Logger from "../logger";
 import nodemailer, { Transporter } from "nodemailer";
 const transporter = nodemailer.createTransport({
-  service: "gmail",
   host: "smtp.gmail.com",
-  port: 465,
+  port: 587,
   auth: {
     user: process.env.NODE_MAILER_AUTH_EMAIL,
     pass: process.env.NODE_MAILER_AUTH_PASSWORD
   },
-  debug: process.env.NODE_ENV === "production",
+  debug: true,
   connectionTimeout: 10000,
   greetingTimeout: 10000,
   socketTimeout: 10000,
